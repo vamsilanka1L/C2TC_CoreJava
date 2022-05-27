6f772d1fc7fcf2396ae7b83bd6c058c3d69e3442
vamsi <vamsi@LAPTOP-002ARLUB>
2022-05-27 21:43:52 +0530

    First Commit

---
+++ operators/src/operators/operator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class operator {
+	public static void main(String[] args) {
+		int a=89,b=79,q,w;
+		q=a+b;
+		w=a-b;
+		double e=(double)a/b;
+		int u=a%b;
+		System.out.println(q);
+		System.out.println(w);
+		System.out.println(e);
+		System.out.println(u);
+		
+		
+		
+	}
+
+}
